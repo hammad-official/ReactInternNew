2c996e3db109036670dbbea51866a0101cc4d966
Your Name <abdulhakimsabir313@gmail.com>
2021-11-29 21:56:47 +0500

    Add dashboard

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,56 @@
 import { Button } from "antd"
 import { Link } from "react-router-dom"
+import 'antd/dist/antd.css';
+import { List, Avatar } from 'antd';
 
+
+const data = [
+    {
+      title: 'Team Member 1',
+    },
+    {
+      title: 'Team Member 2',
+    },
+    {
+      title: 'Team Member 3',
+    },
+    {
+      title: 'Team Member 4',
+    },
+    {
+      title: 'Team Member 5',
+    },
+    {
+      title: 'Team Member 6',
+    },
+    {
+      title: 'Team Member 7',
+    },
+  ];
 const Home = () =>{
     return(
         <>
-        <Button type="primary"><Link to="signin">Home to Signin</Link></Button>
-        <h1>Home</h1>
+<h1> Welcome to the Dashboard </h1>
+  <div className = 'box'>   
+  
+  <List className = 'list'
+  
+    itemLayout="horizontal"
+    dataSource={data}
+    renderItem={item => (
+      <List.Item>
+        <List.Item.Meta
+          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+          title={<a href="https://ant.design">{item.title}</a>}
+          description="Name :" 
+           
+          
+        />
+      </List.Item>
+    )}
+  />,
+  </div>
+
         </>
     );
 };
---
+++ src/components/Teams.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+
+const Teams = () =>{
+    return(
+        <>
+        <Button type="primary"><Link to="signin">Home to Signin</Link></Button>
+        <h1>Hello World</h1>
+        </>
+    );
+};
+export default Teams;
